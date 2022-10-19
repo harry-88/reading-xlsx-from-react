2b29cb64d31c61540a1f65047a4205eee6ba893a
harry-88 <harisbro88@gmail.com>
2022-10-19 15:11:20 +0500

    import excel file in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import * as XLSX from 'xlsx'
 
 function App() {
+
+  const readExcel = async(file) => {
+    try {
+      const promise = await new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.readAsArrayBuffer(file)
+        fileReader.onload = (e) => {
+          const bufferReader = e.target.result
+
+          const wb = XLSX.read(bufferReader, { type: 'buffer' });
+          const wbSheetName = wb.SheetNames[0]
+          const ws = wb.Sheets[wbSheetName];
+          const data = XLSX.utils.sheet_to_json(ws)
+          resolve(data)
+
+        };
+        FileReader.onError = (error) => {
+          reject(error)
+        }
+      })
+
+
+
+      console.log("promise is here ",promise)
+
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='file' onChange={(e) => {
+        const file = e.target.files[0];
+
+        readExcel(file)
+      }} />
     </div>
   );
 }
